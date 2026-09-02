c0391ef3deb4d8f3e738fca69d128dbda3fc4283
stackpillar <omondiedward616@gmail.com>
2026-09-02 06:54:38 +0300

    commit

---
+++ nyumba254/src/index.js
@@ -0,0 +1,16 @@
+export default {
+  async fetch(request, env) {
+    const response = await env.ASSETS.fetch(request);
+
+    if (response.status === 404) {
+      const notFoundUrl = new URL('/404.html', request.url);
+      const notFoundResponse = await env.ASSETS.fetch(new Request(notFoundUrl, request));
+      return new Response(notFoundResponse.body, {
+        status: 404,
+        headers: notFoundResponse.headers,
+      });
+    }
+
+    return response;
+  },
+};
