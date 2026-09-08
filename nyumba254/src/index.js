e3887160e2a8ff85b40baaa23b48285b16b035e7
Edward Omondi <omondiedward616@gmail.com>
2026-09-08 09:35:21 +0300

    Route admin.nyumba254.com to admin dashboard

--- nyumba254/src/index.js
+++ nyumba254/src/index.js
@@ -1,5 +1,12 @@
 export default {
   async fetch(request, env) {
+    const url = new URL(request.url);
+
+    if (url.hostname === 'admin.nyumba254.com' && (url.pathname === '/' || url.pathname === '')) {
+      url.pathname = '/nk-ctrl-7f3k2x9';
+      return env.ASSETS.fetch(new Request(url, request));
+    }
+
     const response = await env.ASSETS.fetch(request);
 
     if (response.status === 404) {
